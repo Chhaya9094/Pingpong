0153eaa695ecc779ee8dbac0ff9f72987ad47a83
Chhaya9094 <45236944+Chhaya9094@users.noreply.github.com>
2018-12-04 00:20:23 +0530

    Create pingPong.js

---
+++ pingPong.js
@@ -0,0 +1,180 @@
+// RequestAnimFrame: a browser API for getting smooth animations
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame       || 
+		window.webkitRequestAnimationFrame || 
+		window.mozRequestAnimationFrame    || 
+		window.oRequestAnimationFrame      || 
+		window.msRequestAnimationFrame     ||  
+		function( callback ){
+			return window.setTimeout(callback, 1000 / 60);
+		};
+})();
+
+window.cancelRequestAnimFrame = ( function() {
+	return window.cancelAnimationFrame          ||
+		window.webkitCancelRequestAnimationFrame    ||
+		window.mozCancelRequestAnimationFrame       ||
+		window.oCancelRequestAnimationFrame     ||
+		window.msCancelRequestAnimationFrame        ||
+		clearTimeout
+} )();
+
+
+// Initialize canvas and required variables
+var canvas = document.getElementById("canvas"),
+		ctx = canvas.getContext("2d"), // Create canvas context
+		W = window.innerWidth, // Window's width
+		H = window.innerHeight, // Window's height
+		particles = [], // Array containing particles
+		ball = {}, // Ball object
+		paddles = [2], // Array containing two paddles
+		mouse = {}, // Mouse object to store it's current position
+		points = 0, // Varialbe to store points
+		fps = 60, // Max FPS (frames per second)
+		particlesCount = 20, // Number of sparks when ball strikes the paddle
+		flag = 0, // Flag variable which is changed on collision
+		particlePos = {}, // Object to contain the position of collision 
+		multipler = 1, // Varialbe to control the direction of sparks
+		startBtn = {}, // Start button object
+		restartBtn = {}, // Restart button object
+		over = 0, // flag varialbe, cahnged when the game is over
+		init, // variable to initialize animation
+		paddleHit;
+
+// Add mousemove and mousedown events to the canvas
+canvas.addEventListener("mousemove", trackPosition, true);
+canvas.addEventListener("mousedown", btnClick, true);
+
+// Initialise the collision sound
+collision = document.getElementById("collide");
+
+// Set the canvas's height and width to full screen
+canvas.width = W;
+canvas.height = H;
+
+// Function to paint canvas
+function paintCanvas() {
+	ctx.fillStyle = "black";
+	ctx.fillRect(0, 0, W, H);
+}
+
+// Function for creating paddles
+function Paddle(pos) {
+	// Height and width
+	this.h = 5;
+	this.w = 150;
+	
+	// Paddle's position
+	this.x = W/2 - this.w/2;
+	this.y = (pos == "top") ? 0 : H - this.h;
+	
+}
+
+// Push two new paddles into the paddles[] array
+paddles.push(new Paddle("bottom"));
+paddles.push(new Paddle("top"));
+
+// Ball object
+ball = {
+	x: 50,
+	y: 50, 
+	r: 5,
+	c: "white",
+	vx: 4,
+	vy: 8,
+	
+	// Function for drawing ball on canvas
+	draw: function() {
+		ctx.beginPath();
+		ctx.fillStyle = this.c;
+		ctx.arc(this.x, this.y, this.r, 0, Math.PI*2, false);
+		ctx.fill();
+	}
+};
+
+
+// Start Button object
+startBtn = {
+	w: 100,
+	h: 50,
+	x: W/2 - 50,
+	y: H/2 - 25,
+	
+	draw: function() {
+		ctx.strokeStyle = "white";
+		ctx.lineWidth = "2";
+		ctx.strokeRect(this.x, this.y, this.w, this.h);
+		
+		ctx.font = "18px Arial, sans-serif";
+		ctx.textAlign = "center";
+		ctx.textBaseline = "middle";
+		ctx.fillStlye = "white";
+		ctx.fillText("Start", W/2, H/2 );
+	}
+};
+
+// Restart Button object
+restartBtn = {
+	w: 100,
+	h: 50,
+	x: W/2 - 50,
+	y: H/2 - 50,
+	
+	draw: function() {
+		ctx.strokeStyle = "white";
+		ctx.lineWidth = "2";
+		ctx.strokeRect(this.x, this.y, this.w, this.h);
+		
+		ctx.font = "18px Arial, sans-serif";
+		ctx.textAlign = "center";
+		ctx.textBaseline = "middle";
+		ctx.fillStlye = "white";
+		ctx.fillText("Restart", W/2, H/2 - 25 );
+	}
+};
+
+// Function for creating particles object
+function createParticles(x, y, m) {
+	this.x = x || 0;
+	this.y = y || 0;
+	
+	this.radius = 1.2;
+	
+	this.vx = -1.5 + Math.random()*3;
+	this.vy = m * Math.random()*1.5;
+}
+
+// Draw everything on canvas
+function draw() {
+	paintCanvas();
+	for(var i = 0; i < paddles.length; i++) {
+		p = paddles[i];
+		
+		ctx.fillStyle = "white";
+		ctx.fillRect(p.x, p.y, p.w, p.h);
+	}
+	
+	ball.draw();
+	update();
+}
+
+// Function to increase speed after every 5 points
+function increaseSpd() {
+	if(points % 4 == 0) {
+		if(Math.abs(ball.vx) < 15) {
+			ball.vx += (ball.vx < 0) ? -1 : 1;
+			ball.vy += (ball.vy < 0) ? -2 : 2;
+		}
+	}
+}
+
+// Track the position of mouse cursor
+function trackPosition(e) {
+	mouse.x = e.pageX;
+	mouse.y = e.pageY;
+}
+
+}
+
+// Show the start screen
+startScreen();
